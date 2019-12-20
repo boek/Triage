c15490d8f9fb514a62935101c8e3a00b63a457b1
Jeff Boek <jeff@jeffboek.com>
2019-12-19 20:03:43 -0800

    Initial Commit

---
+++ Triage/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Triage
+//
+//  Created by Jeff Boek on 12/19/19.
+//  Copyright © 2019 Jeff Boek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
